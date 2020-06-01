f945d4db1e93e8f8daa807dae635b9d8827fa02e
xufei <xufei20160601@163.com>
2020-06-01 11:26:52 +0800

    l_005最长回文子串

---
+++ src/main/java/l_005最长回文子串/Solution.java
@@ -0,0 +1,67 @@
+package l_005最长回文子串;
+
+/**
+ * Filename:    Solution
+ * Description:
+ * Copyright:   Copyright (c) 2016-2017 All Rights Reserved.
+ * Company:     tianli.
+ *
+ * @author: xufei
+ * @version: 1.0
+ * Create at:   2020年06月01日 11:21
+ * Modification History:
+ * Date          Author      Version     Description
+ * ------------------------------------------------------------------
+ * 2020年06月01日 xufei     1.0         1.0 Version
+ */
+public class Solution {
+
+
+    /**
+     * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入: "babad"
+     * 输出: "bab"
+     * 注意: "aba" 也是一个有效答案。
+     * 示例 2：
+     * <p>
+     * 输入: "cbbd"
+     * 输出: "bb"
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("adadadalldaiwqjnvjnfq"));
+    }
+}
+
